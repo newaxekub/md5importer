1fec2961e41131e44ecff7fd07bcaf2b23ed21cf
neakor <neakor@d01af594-a947-0410-a6af-25c1305c5d8c>
2008-07-23 16:01:24 +0000

    Switched to jME2.0 and MD5Importer2.0.

--- MD5Viewer/src/com/md5viewer/MD5Viewer.java
+++ MD5Viewer/src/com/md5viewer/MD5Viewer.java
@@ -1,5 +1,6 @@
 package com.md5viewer;
 
+import java.awt.Font;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -8,13 +9,15 @@ import java.net.URL;
 import java.util.logging.Logger;
 
 import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.jme.app.SimpleGame;
 import com.jme.input.KeyBindingManager;
 import com.jme.input.KeyInput;
 import com.jme.scene.Controller;
+import com.jme.util.resource.MultiFormatResourceLocator;
 import com.jme.util.resource.ResourceLocatorTool;
-import com.jme.util.resource.SimpleResourceLocator;
 import com.md5viewer.enumn.EFileType;
 import com.model.md5.ModelNode;
 import com.model.md5.importer.MD5Importer;
@@ -25,7 +28,7 @@ import com.model.md5.importer.MD5Importer;
  * 
  * @author Yi Wang (Neakor)
  * @version Creation date: 05-21-08 11:30
- * @version Modified date: 05-28-08 12:11
+ * @version Modified date: 07-23-08 11:58
  */
 public class MD5Viewer extends SimpleGame{
 	/**
@@ -63,18 +66,67 @@ public class MD5Viewer extends SimpleGame{
 		app.anim = app.selectFile(EFileType.Animation);
 		app.start();
 	}
-	
+
 	/**
 	 * Constructor of <code>MD5Viewer</code>.
 	 */
 	public MD5Viewer() {
+		this.initTheme();
+		this.setConfigShowMode(ConfigShowMode.AlwaysShow);
 		this.logger = Logger.getLogger(MD5Viewer.class.toString());
 		this.chooser = new JFileChooser();
 		this.speed = 1;
 		this.scale = 1;
-		this.setDialogBehaviour(MD5Viewer.ALWAYS_SHOW_PROPS_DIALOG, (URL)null);
 	}
-	
+
+	/**
+	 * Initialize the Swing theme.
+	 */
+	private void initTheme() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		UIManager.put("Button.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ToggleButton.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("RadioButton.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("CheckBox.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ColorChooser.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ComboBox.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Label.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("List.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("MenuBar.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("MenuItem.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("RadioButtonMenuItem.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("CheckBoxMenuItem.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Menu.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("PopupMenu.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("OptionPane.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Panel.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ProgressBar.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ScrollPane.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Viewport.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TabbedPane.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Table.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TableHeader.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TextField.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("PasswordField.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TextArea.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TextPane.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("EditorPane.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("TitledBorder.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ToolBar.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("ToolTip.font", new Font("Tohama",Font.PLAIN,11));
+		UIManager.put("Tree.font", new Font("Tohama",Font.PLAIN,11));
+	}
+
 	/**
 	 * Select a <code>File</code> and return its <code>URL</code>.
 	 * @param type The intended <code>FileType</code> of the <code>File</code>.
@@ -109,7 +161,7 @@ public class MD5Viewer extends SimpleGame{
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Validate the given file name with <code>FileType</code>.
 	 * @param type The <code>FileType</code> standard.
@@ -137,7 +189,7 @@ public class MD5Viewer extends SimpleGame{
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		// Load mesh and animation.
+			// Load mesh and animation.
 		} else if(this.mesh != null && this.anim != null) {
 			try {
 				MD5Importer.getInstance().load(this.mesh, "Mesh", this.anim, "Animation", Controller.RT_WRAP);
@@ -149,7 +201,7 @@ public class MD5Viewer extends SimpleGame{
 		this.rootNode.attachChild(model);
 		MD5Importer.getInstance().cleanup();
 	}
-	
+
 	/**
 	 * Setup the additional key bindings.
 	 */
@@ -159,16 +211,17 @@ public class MD5Viewer extends SimpleGame{
 		KeyBindingManager.getKeyBindingManager().set("scaleup", KeyInput.KEY_3);
 		KeyBindingManager.getKeyBindingManager().set("scaledown", KeyInput.KEY_4);
 	}
-	
+
 	/**
 	 * Override the <code>Texture</code> key location.
 	 */
 	private void overrideTextureKey() {
 		try {
-			ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, new SimpleResourceLocator(this.mesh));
+			MultiFormatResourceLocator locator = new MultiFormatResourceLocator(this.mesh, new String[]{".tga", ".bmp", ".jpg", ".png"});
+			ResourceLocatorTool.addResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, locator);
 		} catch (URISyntaxException e) {e.printStackTrace();}
 	}
-	
+
 	protected void simpleUpdate() {
 		if(KeyBindingManager.getKeyBindingManager().isValidCommand("speedup", false)) {
 			this.speed += 1f;
