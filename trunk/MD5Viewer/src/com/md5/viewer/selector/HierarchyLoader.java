28927d00e983cf6a84da65bda9001e5ef06385c3
neakor <neakor@d01af594-a947-0410-a6af-25c1305c5d8c>
2008-11-24 15:24:12 +0000

    Finished implementing hierarchy loader.

--- trunk/MD5Viewer/src/com/md5/viewer/selector/HierarchyLoader.java
+++ trunk/MD5Viewer/src/com/md5/viewer/selector/HierarchyLoader.java
@@ -1,7 +1,10 @@
 package com.md5.viewer.selector;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * <code>HierarchyLoader</code> defines a file loading unit that is responsible
@@ -10,25 +13,34 @@ import java.util.List;
  * @author Yi Wang (Neakor)
  * @author Tim Poliquin (Weenahmen)
  * @version Creation date: 11-23-2008 23:19 EST
- * @version Modified date: 11-23-2008 23:19 EST
+ * @version Modified date: 11-24-2008 10:23 EST
  */
 public class HierarchyLoader {
+	/**
+	 * The <code>String</code> of loaded base directory.
+	 */
+	private String dir;
 
 	/**
 	 * Constructor of <code>HierarchyLoader</code>.
 	 */
-	public HierarchyLoader() {
-		// TODO
-	}
+	public HierarchyLoader() {}
 	
 	/**
 	 * Load the hierarchy information from the file linked by given URL.
 	 * @param file The <code>URL</code> links to the file to be loaded.
 	 * @return The loaded <code>List</code> of <code>String</code> hierarchy.
+	 * @throws IOException If input scan is interrupted.
 	 */
-	public List<String> load(URL file) {
-		// TODO Load into the hierarchy.
-		return null;
+	public List<String> load(URL file) throws IOException {
+		ArrayList<String> list = new ArrayList<String>();
+		Scanner scanner = new Scanner(file.openStream());
+		while(scanner.hasNextLine()) {
+			list.add(scanner.nextLine());
+		}
+		String raw = file.getPath().replaceAll("%20", " ");
+		this.dir = raw.substring(0, raw.lastIndexOf("/") + 1);
+		return list;
 	}
 	
 	/**
@@ -36,6 +48,6 @@ public class HierarchyLoader {
 	 * @return The <code>String</code> base directory.
 	 */
 	public String getBaseDirectory() {
-		return null;
+		return this.dir;
 	}
 }
