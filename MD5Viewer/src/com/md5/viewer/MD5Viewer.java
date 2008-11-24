822d3644121bb3f409e938941ebcd62f03c8c308
neakor <neakor@d01af594-a947-0410-a6af-25c1305c5d8c>
2008-11-24 04:34:02 +0000

    Updated doc. Added hierarchy loader to load in animated parts hierarchy.

--- MD5Viewer/src/com/md5/viewer/MD5Viewer.java
+++ MD5Viewer/src/com/md5/viewer/MD5Viewer.java
@@ -2,10 +2,20 @@ package com.md5.viewer;
 
 import com.md5.viewer.gui.AnimationSelector;
 
+/**
+ * <code>MD5Viewer</code> defines the concrete implementation of a viewer
+ * unit that allows user to selected one or more animations and view them
+ * in a chain in manual or automatic mode.
+ *
+ * @author Yi Wang (Neakor)
+ * @author Tim Poliquin (Weenahmen)
+ * @version Creation date: 11-23-2008 23:08 EST
+ * @version Modified date: 11-23-2008 23:08 EST
+ */
 public class MD5Viewer {
 
 	/**
-	 * @param args
+	 * Main method.
 	 */
 	public static void main(String[] args) {
 		AnimationSelector selector = new AnimationSelector();
