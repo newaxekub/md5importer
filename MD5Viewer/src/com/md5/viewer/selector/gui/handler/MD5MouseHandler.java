63a98715fab693c5ed0e4d44e20062383c023cc9
neakor <neakor@d01af594-a947-0410-a6af-25c1305c5d8c>
2008-11-24 19:59:02 +0000

    Finished cancel exit.

--- MD5Viewer/src/com/md5/viewer/selector/gui/handler/MD5MouseHandler.java
+++ MD5Viewer/src/com/md5/viewer/selector/gui/handler/MD5MouseHandler.java
@@ -20,7 +20,7 @@ import com.md5.viewer.selector.gui.enumn.EButton;
  * @author Yi Wang (Neakor)
  * @author Tim Poliquin (Weenahmen)
  * @version Creation date: 11-24-2008 12:52 EST
- * @version Modified date: 11-24-2008 14:56 EST
+ * @version Modified date: 11-24-2008 14:58 EST
  */
 public class MD5MouseHandler extends MouseAdapter {
 	/**
@@ -65,7 +65,7 @@ public class MD5MouseHandler extends MouseAdapter {
 				this.seletor.removeAnimation(this.seletor.getGUI().getSelectedAnim());
 				break;
 			case OK: break;
-			case Cancel: break;
+			case Cancel: System.exit(0); break;
 			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
