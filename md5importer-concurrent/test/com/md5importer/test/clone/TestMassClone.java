4b9488c54a44018b11cd23f41048ea31d4abb232
Neakor <Neakor@d01af594-a947-0410-a6af-25c1305c5d8c>
2009-07-01 22:27:55 +0000

    Added a test for massive cloning.

---
+++ md5importer-concurrent/test/com/md5importer/test/clone/TestMassClone.java
@@ -0,0 +1,98 @@
+package com.md5importer.test.clone;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.jme.app.SimpleGame;
+import com.jme.scene.Spatial;
+import com.md5importer.MD5Importer;
+import com.md5importer.control.MD5AnimController;
+import com.md5importer.control.MD5NodeController;
+import com.md5importer.interfaces.model.IMD5Anim;
+import com.md5importer.interfaces.model.IMD5Node;
+import com.md5importer.test.util.ThreadedUpdater;
+
+public class TestMassClone extends SimpleGame {
+	
+	private final MD5Importer importer;
+	private final ThreadedUpdater updater;
+
+	private final int size;
+	private final IMD5Node[] nodes;
+	
+	private IMD5Node originNode;
+	private IMD5Anim originAnim;
+	
+	public TestMassClone() {
+		this.importer = new MD5Importer();
+		this.updater = new ThreadedUpdater(24);
+		this.size = 20;
+		this.nodes = new IMD5Node[this.size];
+	}
+
+	@Override
+	protected void simpleInitGame() {
+		this.createSet();
+		for(int i = 0; i < this.size; i++) this.createClone(i);
+		this.updater.start();
+	}
+	
+	@Override
+	protected void simpleUpdate() {
+		super.simpleUpdate();
+		for(int i = 0; i < this.size; i++) this.nodes[i].swapBuffers();
+	}
+	
+	private void createSet() {
+		try {
+			this.originNode = this.loadMesh();
+			this.originAnim = this.loadAnim();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void createClone(int index) {
+		final IMD5Node node = this.originNode.clone();
+		final IMD5Anim anim = this.originAnim.clone();
+		this.initControllers(node, anim);
+		((Spatial)node).setLocalTranslation(30*index, 0, 0);
+		this.nodes[index] = node;
+		this.rootNode.attachChild((Spatial)node);
+	}
+	
+	private IMD5Node loadMesh() throws IOException {
+		try {
+			final URL url = this.getClass().getClassLoader().getResource("com/md5importer/test/data/marine.md5mesh");
+			return this.importer.loadMesh(url, "Mesh");
+		} finally {
+			this.importer.cleanup();
+		}
+	}
+	
+	private IMD5Anim loadAnim() throws IOException {
+		try {
+			final URL url = this.getClass().getClassLoader().getResource("com/md5importer/test/data/marine.md5anim");
+			return this.importer.loadAnim(url, "Anim");
+		} finally {
+			this.importer.cleanup();
+		}
+	}
+	
+	private void initControllers(final IMD5Node node, final IMD5Anim anim) {
+		final MD5NodeController nodeController = new MD5NodeController(node);
+		final MD5AnimController animController = new MD5AnimController(anim);
+		anim.register(nodeController);
+		nodeController.setActiveAnim(anim);
+		this.updater.addController(animController);
+	}
+	
+	protected void cleanup() {
+		super.cleanup();
+		this.updater.stop();
+	}
+
+	public static void main(String[] args) {
+		new TestMassClone().start();
+	}
+}
